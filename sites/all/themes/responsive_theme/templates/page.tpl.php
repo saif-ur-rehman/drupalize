a0cb08e9737f5aecc798638283317a54d76ccdf4
Saif-ur-Rehman <saif54624@yahoo.com>
2016-08-26 10:46:33 +0500

    change css

--- sites/all/themes/responsive_theme/templates/page.tpl.php
+++ sites/all/themes/responsive_theme/templates/page.tpl.php
@@ -139,13 +139,13 @@
 
     <section<?php print $content_column_class; ?>>
       <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+        <div class="highlighted"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
       <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
+        <h1 class="page-header"><?php // print $title; ?></h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>
